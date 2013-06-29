0299cb97eebd43d02215e109b331a7ce7d3173a6
Darius Scerbavicius <darius.scerb@gmail.com>
2013-06-29 19:05:45 +0100

    Beginnings of an actual implementation using Twisted

--- src/chord_node.py
+++ src/chord_node.py
@@ -1,127 +1,118 @@
 from twisted.protocols.basic import LineReceiver
 from twisted.internet import reactor, protocol
+from twisted.internet.defer import Deferred, succeed
+from twisted.internet.protocol import Protocol, ClientFactory, ServerFactory
+from twisted.protocols.basic import NetstringReceiver
 import argparse
 import md5
+import collections
 
 FILENAME = "to_send.test"
-def hash(s):
-	return long(md5.new(s).hexdigest(), 16)
 
-def read_bytes_from_file(file, chunk_size = 8100):
-	with open(file, 'rb') as file:
-		while True:
-			chunk = file.read(chunk_size)
-			
-			if chunk:
-					yield chunk
-			else:
-				break
+def Hash(s):
+  return int(long(md5.new(s).hexdigest(), 16) % 160)
 
-class ChordNodeProtocol(LineReceiver):
-	delimiter = '\n'
-	def __init__(self, factory, shared_data):
-		self.factory = factory
-		self.shared_data = shared_data
-		self.hash = None
-		self.file_handler = None
+class ChordClientProtocol(NetstringReceiver):
 
-	def connectionMade(self):
-		print('someone connected')
-		#self.sendLine('requestHash')
-		self.sendLine('requestFile\n')
-		pass
+  value = ''
 
-	def lineReceived(self, line):
-		print line
-		if line.startswith('requestHash'):
-			address = self.transport.getHost()
-			address_hash = hash(address.host + ':' + str(address.port))
-			self.sendLine('receiveHash ' + str(address_hash) +'\n')
-		elif line.startswith('receiveHash'):
-			self.hash = line.split()[1]
-			print("got hash: " + self.hash)
-		elif line.startswith('requestFile'):
-			#if self.shared_data.receiver: return
-			self.transport.write('receiveFile\n')
-			print 'raw'
-			self.setRawMode()
-			print 'writing...'
+  def connectionMade(self):
+    self.sendString("retrieve_value" + '.' + self.factory.key)
 
-			for bytes in read_bytes_from_file(FILENAME):
-				self.transport.write(bytes)
+  def dataReceived(self, data):
+    self.value += data 
 
-			self.transport.write('\r\n')	
-			self.setLineMode()
-		elif line.startswith('receiveFile'):
-			#if not self.shared_data.receiver:
-			#	return
-			print 'receiving file'
-			self.setRawMode()
+  def connectionLost(self, reason):
+    self.factory.ValueReceived(self.value)
 
-	def rawDataReceived(self, data):
-		filename = self.shared_data.filename
-		
-		print 'Receiving file chunk (%d KB)' % (len(data))
-		
-		if not self.file_handler:
-			self.file_handler = open(self.shared_data.filename, 'wb')
-			
-		if data.endswith('\r\n'):
-			# Last chunk
-			data = data[:-2]
-			self.file_handler.write(data)
-			self.setLineMode()
-			
-			self.file_handler.close()
-			self.file_handler = None
-		else:
-			self.file_handler.write(data)
 
-class NodeData(object):
-	def __init__(self, filename):
-		self.test = []
-		self.filename = filename
-		self.receiver = False
+class ChordServerFactory(ServerFactory):
 
-class ServerNodeFactory(protocol.ServerFactory):
-	protocol = ChordNodeProtocol
-	
-	def __init__(self, shared_data):
-		self.shared_data = shared_data
+    protocol = Protocol
 
-	def buildProtocol(self, addr):
-		return ChordNodeProtocol(self, self.shared_data)
+    def __init__(self, service):
+        self.service = service
 
-class ClientNodeFactory(protocol.ClientFactory):
-	protocol = ChordNodeProtocol
-	def __init__(self, server_node_factory):
-		self.server_node_factory = server_node_factory
 
-	def buildProtocol(self, addr):
-		return ChordNodeProtocol(self, self.server_node_factory.shared_data)
+class ChordClientFactory(ClientFactory):
+
+  protocol = ChordClientProtocol
+
+  def __init__(self, key, deferred):
+    self.key = key
+    self.deferred = deferred
+
+  def ValueReceived(self, value):
+    if self.deferred is not None:
+      d, self.deferred = self.deferred, None
+      d.callback(value)
+
+
+Address = collections.namedtuple('Address', ['host', 'port'])
+
+
+def HashAddress(address):
+  return hash(str(address.host) + str(address.port))
+  
+
+class ChordService(object):
+  
+  def __init__(self):
+    self.data = {}
+    self.routing_table = {}
+
+  def AddToRoutingTable(address):
+    h = HashAddress(address)
+    self.routing_table[h] = address
+    print("Added {} to routing table.".format(address))
+
+  def StoreValue(self, key, value):
+    self.data[key] = value
+    print("Stored key: {}, value: {}.".format(key, value))
+    
+  def GetValue(self, key):
+    # check if value is among the values you hold
+    if key in self.data:
+      return succeed(self.data[key])
+
+    # if it is not, look at your routing table
+    deferred = Deferred()
+    factory = ChordClientFactory(key, deferred)
+    address = routing_table[int(floor(log(key, 2)))]
+    reactor.connectTCP(address.host, address.port, factory)
+    return factory.deferred
 
 def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--port')
-	parser.add_argument('--filename', default="test.file")
-	parser.add_argument('--connect', default=None)
-	parser.add_argument('--receive', action='store_true')
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--port')
+  parser.add_argument('--store')
+  parser.add_argument('--retrieve')
+  parser.add_argument('--connect', default=None)
 
-	args = parser.parse_args()
-	if args.receive:
-		print 'i am a receiver'
-	shared_data = NodeData(args.filename)
-	shared_data.receiver = args.receive
-	f = ServerNodeFactory(shared_data)
-	port = int(args.port)
-	reactor.listenTCP(port, f)
-	if (args.connect):
-		host, port = args.connect.split(':')
-		port = int(port)
-		f_client = ClientNodeFactory(f)
-		
-		reactor.connectTCP(host, port, f_client)
-	reactor.run()
+  args = parser.parse_args()
+  port = int(args.port)
+
+  service = ChordService()
+
+  f = ChordServerFactory(service)
+  reactor.listenTCP(port, f)
+  if (args.connect):
+    host, port = args.connect.split(':')
+    service.AddToRoutingTable(Address(host, int(port)))
+
+  if (args.store):
+    key, value = args.store.split(':')
+    service.StoreValue(key, value)
+    
+  if (args.retrieve):
+    def EchoValue(value):
+      print('Retrieved value: {}'.format(value))
+    d = service.GetValue(args.retrieve)
+    d.addCallback(EchoValue)
+    
+  reactor.run()
 
 if __name__ == '__main__':
-	main()
+  main()
+
+  
