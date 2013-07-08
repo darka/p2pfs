c82f63ef241afa01f0c08647c8eb7a37180d900c
Darius Scerbavicius <darius.scerb@gmail.com>
2013-07-08 22:53:08 +0100

    Switched to using Perspective Broker

--- src/chord_node.py
+++ src/chord_node.py
@@ -4,89 +4,22 @@ from twisted.internet.address import IPv4Address
 from twisted.internet.defer import Deferred, succeed
 from twisted.internet.protocol import Protocol, ClientFactory, ServerFactory
 from twisted.protocols.basic import NetstringReceiver
+from twisted.spread import pb
 import argparse
 import md5
 import collections
 import math
 
 
-def Echo(s):
-  print(s)
-
 def Hash(s):
   return int(long(md5.new(s).hexdigest(), 16) % 160)
-
-
-class ChordServerProtocol(NetstringReceiver):
-
-  def connectionMade(self):
-    print("Someone connected.")
-
-  def stringReceived(self, request):
-    print("Received a request: {}.".format(request))
-
-    if '.' not in request: # bad request
-        self.transport.loseConnection()
-        return
-
-    req, arg = request.split('.', 1)
-
-    d = self.factory.HandleRequest(req)
-    d.addCallback(lambda ret: self.sendString(str(ret)))
-    d.addCallback(lambda _: self.transport.loseConnection)
-    d.callback(int(arg))
-
-
-class ChordServerFactory(ServerFactory):
-
-  protocol = ChordServerProtocol
-
-  def __init__(self, service):
-    self.service = service
-
-  def HandleRequest(self, req):
-    d = Deferred()
-    if req == 'retrieve_value':
-      d.addCallback(self.service.GetValue)
-    elif req == 'who_am_i':
-      d.addCallback(self.service.GetValue)
-    return d
-
-
-class ChordClientProtocol(NetstringReceiver):
-
-  value = ''
-
-  def connectionMade(self):
-    print("Asking host for key: {}.".format(self.factory.key))
-    self.sendString("retrieve_value" + '.' + self.factory.key)
-
-  def stringReceived(self, data):
-    self.factory.ValueReceived(data)
-
-  def connectionLost(self, reason):
-    print('Connection lost.')
-
-
-class ChordClientFactory(ClientFactory):
-
-  protocol = ChordClientProtocol
-
-  def __init__(self, key, deferred):
-    self.key = key
-    self.deferred = deferred
-
-  def ValueReceived(self, value):
-    if self.deferred is not None:
-      d, self.deferred = self.deferred, None
-      d.callback(value)
 
 
 def HashAddress(address):
   return Hash(str(address.host) + str(address.port))
   
 
-class ChordService(object):
+class ChordService(pb.Root):
   
   def __init__(self):
     self.me = None
@@ -102,6 +35,9 @@ class ChordService(object):
     self.data[int(key)] = value
     print("Stored key: {}, value: {}.".format(key, value))
     
+  def remote_GetValue(self, key):
+    return self.GetValue(int(key))
+
   def GetValue(self, key):
     print('Retrieving value with key: {}.'.format(key))
     # check if value is among the values you hold
@@ -110,13 +46,16 @@ class ChordService(object):
 
     # if it is not, look at your routing table
     deferred = Deferred()
-    factory = ChordClientFactory(key, deferred)
+    #factory = ChordClientFactory(key, deferred)
+    factory = pb.PBClientFactory()
     #address_hash = int(math.floor(math.log(int(key), 2)))
     #print("address hash: {}".format(address_hash)
     #address = self.routing_table[address_hash]
     address = self.routing_table[int(key)]
     reactor.connectTCP(address.host, address.port, factory)
-    return factory.deferred
+    d = factory.getRootObject()
+    d.addCallback(lambda object: object.callRemote("GetValue", key))
+    return d
 
 
 def main():
@@ -145,7 +84,7 @@ def main():
     d = service.GetValue(args.retrieve)
     d.addCallback(EchoValue)
 
-  f = ChordServerFactory(service)
+  f = pb.PBServerFactory(service)
   reactor.listenTCP(port, f)
     
   reactor.run()
@@ -153,4 +92,3 @@ def main():
 if __name__ == '__main__':
   main()
 
-  
