936329ce6a83e40454746f99ec90a60b2d6df76a
Darius Scerbavicius <darius.scerb@gmail.com>
2013-12-12 19:37:05 +0000

    WIP reimplementing database to improve speed

--- src/file_database.py
+++ src/file_database.py
@@ -3,154 +3,135 @@ import time
 import os
 import sqlite3
 
+class FileObject(object):
+  attr_fields = 'st_atime, st_ctime, st_mode, st_mtime, st_nlink, st_size'.split(', ')
+  def __init__(self):
+    self.attrs = {}
+    self.contents = {}
+
 class FileDatabase(object):
   def __init__(self, logger, key, filename, new=False):
     self.key = key
     self.db_filename = filename
     self.new = new
     self.l = logger
-    self.attr_fields = 'st_atime, st_ctime, st_mode, st_mtime, st_nlink, st_size'.split(', ')
 
   def ready(self, file_service):
     self.file_service = file_service
-    self.conn = sqlite3.connect(self.db_filename)
-    if self.new:
-      self.create_tables()
+    if self.new or not os.path.exists(self.db_filename):
+      self.data = {}
+      self.update_db_time()
+    else:
+      self.data = pickle.load(open(self.db_filename))
     
-  def execute(self, command):
-    self.l.log('SQL', command)
-    c = self.conn.cursor()
-    c.execute(command)
-    return c
+  #def create_tables(self):
+  #  try:
+  #    self.execute('''DROP TABLE files''')
+  #  except sqlite3.OperationalError:
+  #    self.l.log('DB', 'Could not drop table')
+  #  self.execute(
+  #        "CREATE TABLE files ("
+  #        "pub_key text, "
+  #        "filename text, "
+  #        "path text, "
+  #        "st_mode integer, "
+  #        "st_uid integer, "
+  #        "st_gid integer, "
+  #        "st_atime integer DEFAULT 0, "
+  #        "st_mtime integer DEFAULT 0, "
+  #        "st_ctime integer DEFAULT 0, "
+  #        "st_nlink integer DEFAULT 1, "
+  #        "st_size integer DEFAULT 0, "
+  #        "is_internal integer DEFAULT 0, "
+  #        "PRIMARY KEY(path, filename, pub_key))")
+  #  current_time = int(time.time())
+  #  self.execute("INSERT INTO files"
+  #               "(pub_key, is_internal, filename, path, st_mode, st_atime, st_mtime, st_ctime, st_size) "
+  #               "VALUES('{}', 1, 'DB', 'DB', {}, {}, {}, {}, {})".format(
+  #      self.key, 0, current_time, current_time, current_time, 0))
 
-  def commit(self):
-    self.conn.commit()
-    m_time = self.get_db_mtime(self.key)
-    self.file_service.publish_file(self.key, os.path.basename(self.db_filename), self.db_filename, m_time)
-    
-  def create_tables(self):
-    try:
-      self.execute('''DROP TABLE files''')
-    except sqlite3.OperationalError:
-      self.l.log('DB', 'Could not drop table')
-    self.execute(
-          "CREATE TABLE files ("
-          "pub_key text, "
-          "filename text, "
-          "path text, "
-          "st_mode integer, "
-          "st_uid integer, "
-          "st_gid integer, "
-          "st_atime integer DEFAULT 0, "
-          "st_mtime integer DEFAULT 0, "
-          "st_ctime integer DEFAULT 0, "
-          "st_nlink integer DEFAULT 1, "
-          "st_size integer DEFAULT 0, "
-          "is_internal integer DEFAULT 0, "
-          "PRIMARY KEY(path, filename, pub_key))")
-    current_time = int(time.time())
-    self.execute("INSERT INTO files"
-                 "(pub_key, is_internal, filename, path, st_mode, st_atime, st_mtime, st_ctime, st_size) "
-                 "VALUES('{}', 1, 'DB', 'DB', {}, {}, {}, {}, {})".format(
-        self.key, 0, current_time, current_time, current_time, 0))
+  #  self.commit()
 
-    self.commit()
-
+  def get_file_object(public_key, path):
+    path = path.split('/')
+    result = self.data[public_key]
+    for dir_name in path:
+      if dir_name:
+        new_result = result.contents.get(dir_name)
+        if new_result:
+          result = new_result
+        else:
+          return None
+    return result
+      
   def chmod(self, public_key, path, mode):
-    filename = os.path.basename(path)
-    dirname = os.path.dirname(path)
-    c = self.execute("SELECT st_mode FROM files WHERE pub_key='{}' AND path='{}' AND filename='{}'".format(public_key, dirname, filename))
-    old_mode = c.fetchone()[0]
-    old_mode &= 0770000
-    mode = old_mode | mode
-    self.execute("UPDATE files SET st_mode={} WHERE path='{}' AND filename='{}' AND pub_key='{}'".format(
-        mode, dirname, filename, public_key))
+    fobj = get_file_object(public_key, path)
+    fobj.attrs['st_mode'] &= 0770000
+    fobj.attrs['st_mode'] |= mode
     self.update_db_time()
-    self.commit()
 
   def get_file_mtime(self, public_key, path):
-    filename = os.path.basename(path)
-    dirname = os.path.dirname(path)
     self.l.log('DB', 'Retrieving mtime for: {}'.format(filename))
-    c = self.execute("SELECT st_mtime FROM files WHERE pub_key='{}' AND path='{}' AND filename='{}'".format(public_key, dirname, filename))
-    ret = c.fetchone()
-    self.l.log('DB', 'Result: {}'.format(str(ret)))
-    if not ret: 
+    fobj = get_file_object(public_key, path)
+    if not fobj:
       return 0
-    else: 
-      return ret[0]
+    else:
+      return fobj.attrs['st_mtime']
     
   def get_db_mtime(self, public_key):
-    self.l.log('DB', 'Retrieving db mtime')
-    c = self.execute("SELECT st_mtime FROM files WHERE pub_key='{}' AND path='DB' AND filename='DB' AND is_internal=1".format(public_key))
-    ret = c.fetchone()
-    self.l.log('DB', 'Result: {}'.format(str(ret)))
-    if not ret: return 0
-    else: return ret[0]
+    return self.data['current_time']
     
   def update_time(self, public_key, path, atime, mtime):
-    filename = os.path.basename(path)
-    dirname = os.path.dirname(path)
-    self.execute("UPDATE files SET st_atime={}, st_mtime={} WHERE path='{}' AND filename='{}' AND pub_key='{}'".format(
-        atime, mtime, dirname, filename, public_key))
+    fobj = get_file_object(public_key, path)
+    fobj.attrs['st_atime'] = atime
+    fobj.attrs['st_mtime'] = mtime
     self.update_db_time()
-    self.commit()
 
   def update_file_mtime(self, public_key, path):
-    filename = os.path.basename(path)
-    dirname = os.path.dirname(path)
+    fobj = get_file_object(public_key, path)
     current_time = int(time.time())
-    self.execute("UPDATE files SET st_mtime={} WHERE path='{}' AND filename='{}' AND pub_key='{}'".format(
-        current_time, dirname, filename, public_key))
+    fobj.attrs['st_mtime'] = current_time
     self.update_db_time()
-    self.commit()
     return current_time
 
   def update_size(self, public_key, path, size):
-    filename = os.path.basename(path)
-    dirname = os.path.dirname(path)
-    self.execute("UPDATE files SET st_size={} WHERE path='{}' AND filename='{}' AND pub_key='{}'".format(
-        size, dirname, filename, public_key))
+    fobj = get_file_object(public_key, path)
+    fobj.attrs['st_size'] = size
     self.update_db_time()
-    self.commit()
     
   def chown(self, public_key, path, uid, gid):
-    filename = os.path.basename(path)
-    dirname = os.path.dirname(path)
-    self.execute("UPDATE files SET st_uid={}, st_gid={} WHERE path='{}' AND filename='{}' AND pub_key='{}'".format(
-        uid, gid, dirname, filename, public_key))
+    fobj = get_file_object(public_key, path)
+    fobj.attrs['st_uid'] = uid
+    fobj.attrs['st_gid'] = gid
     self.update_db_time()
-    self.commit()
     
   def getattr(self, public_key, path):
-    dirname = os.path.dirname(path)
-    filename = os.path.basename(path)
-    c = self.execute("SELECT st_atime, st_ctime, st_mode, st_mtime, st_nlink, st_size FROM files WHERE pub_key='{}' AND path='{}' AND filename='{}'".format(public_key, dirname, filename))
-    attrs = c.fetchone()
-    if not attrs: 
-      return None
-    return dict(zip(self.attr_fields, attrs))
+    fobj = get_file_object(public_key, path)
+    return fobj.attrs
 
   def rename(self, public_key, old_path, new_path):
-    old_dirname = os.path.dirname(old_path)
-    old_filename = os.path.basename(old_path)
-    new_dirname = os.path.dirname(new_path)
-    new_filename = os.path.basename(new_path)
-    current_time = int(time.time())
-    self.execute("UPDATE files SET path='{}', filename='{}' WHERE path='{}' AND filename='{}'".format(
-        new_dirname, new_filename, old_dirname, old_filename))
+    old_dirname, old_filename = os.path.split(old_path)
+    new_dirname, new_filename = os.path.split(new_path)
+    old_location = get_file_object(public_key, old_dirname)
+    destination = get_file_object(public_key, new_dirname)
+    destination.contents[new_filename] = old_location.contents[old_filename]
+    del old_location.contents[old_filename]
     self.update_db_time()
-    self.commit()
 
   def add_file(self, public_key, path, mode, size):
-    dirname, filename = os.path.split(path)
+    new_file = FileObject()
     current_time = int(time.time())
+    new_file.attrs['st_mode'] = mode
+    new_file.attrs['st_atime'] = current_time
+    new_file.attrs['st_mtime'] = current_time
+    new_file.attrs['st_ctime'] = current_time
+    new_file.attrs['st_size'] = size
+    dirname, filename = os.path.split(path)
     self.execute("INSERT INTO files"
                  "(pub_key, filename, path, st_mode, st_atime, st_mtime, st_ctime, st_size) "
                  "VALUES('{}', '{}', '{}', '{}', '{}', '{}', '{}', {})".format(
         public_key, filename, dirname, S_IFREG | mode, current_time, current_time, current_time, size))
     self.update_db_time()
-    self.commit()
 
   def delete_file(self, public_key, path):
     dirname, filename = os.path.split(path)
@@ -167,8 +148,7 @@ class FileDatabase(object):
     self.commit()
 
   def update_db_time(self):
-    current_time = int(time.time())
-    self.execute("UPDATE files SET st_mtime={}, st_ctime={} WHERE filename='DB' AND path='DB' AND is_internal=1".format(current_time, current_time))
+    self.data['current_time'] = int(time.time())
 
   def add_directory(self, public_key, path, mode):
     current_time = int(time.time())
