3cf551265d60abe9b156a9854862055a45920f62
Darius Scerbavicius <darius.scerb@gmail.com>
2014-02-22 14:15:30 +0000

    Specify commands in a file

--- helper-scripts/multi-server-run.py
+++ helper-scripts/multi-server-run.py
@@ -2,28 +2,35 @@ import threading
 import paramiko
 import sys
 import os
+import argparse
 
 logs_folder = 'logs'
 logs_err_folder = os.path.join(logs_folder, 'err')
 
-def ssh_run(host):
+def ssh_run(host, commands):
   ssh = paramiko.SSHClient()
   ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
   ssh.connect(host)
-  stdin, stdout, stderr = ssh.exec_command('ls')
-
-  out = open(os.path.join(logs_err_folder, host + '.logerr'), 'w')
-  out.write(stderr.read())
-  out.close()
 
   out = open(os.path.join(logs_folder, host + '.log'), 'w')
-  out.write(stdout.read())
+  out_err = open(os.path.join(logs_err_folder, host + '.logerr'), 'w')
+  for command in commands:
+    command = command.strip()
+    if command.startswith('#'):
+      continue
+    stdin, stdout, stderr = ssh.exec_command(command)
+  
+    out.write(stdout.read())
+    out_err.write(stderr.read())
+
+  out_err.close()
   out.close()
 
 def main():
-  if len(sys.argv) != 2:
-    print "Usage: multi-server-run.py [FILE]"
-    sys.exit(1)
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--hosts', required=True)
+  parser.add_argument('--commands', required=True)
+  args = parser.parse_args()
 
   if not os.path.exists(logs_folder):
     os.makedirs(logs_folder)
@@ -32,15 +39,16 @@ def main():
     os.makedirs(logs_err_folder)
 
   hosts = []
-  f = open(sys.argv[1], 'r')
+  f = open(args.hosts, 'r')
   for host in f.readlines():
     hosts.append(host.strip())
   f.close()
 
   threads = []
+  commands = open(args.commands, 'r').readlines()
   for host in hosts:
     print host
-    t = threading.Thread(target=ssh_run, args=(host,))
+    t = threading.Thread(target=ssh_run, args=(host,commands))
     t.start()
     threads.append(t)
 
